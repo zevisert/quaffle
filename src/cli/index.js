99fa5fa88713146d2bb384a862ced0a200a37ca8
Zev Isert <dev@zevisert.ca>
2019-04-08 09:12:29 -0700

    Add variant language support in cli

--- src/cli/index.js
+++ src/cli/index.js
@@ -3,7 +3,7 @@ const glob = require("glob")
 const chalk = require("chalk")
 const enquirer = require("enquirer")
 
-const { python } = require("../language")
+const languages = require("../language")
 
 async function main({glob_path=null, Prompt=enquirer.prompt}) {
 
@@ -28,39 +28,40 @@ async function main({glob_path=null, Prompt=enquirer.prompt}) {
     const files = glob.sync(glob_path)
     
     try {
-        let answers = await Prompt([{
+        let answer = await Prompt([{
             type: 'select',
             name: 'language',
             message: 'Which language are you analyzing?',
             initial: 0,
             choices: [
-                { name: 'python', message: 'Python' },
+                ...Object.values(languages).map(lang => { return {
+                    name: lang.choice_name,
+                    message: lang.choice_name
+                }}),
                 { name: 'other', message: 'Other' }
             ]
         }])
 
-        switch (answers.language) {
-            case "python": {
-                    answers = await Prompt([{
-                        type: 'multiselect',
-                        name: 'rules',
-                        message: 'Which rules would you like to apply?',
-                        choices: python.rule_names
-                    }])
-                    
-                    await python.operations({ files, enabled_rules: answers.rules })
-                    break;
-            }
-            case "other":
-            default: {
-                console.log(chalk.red('Analysis in other languages is a work in progress'))
-                process.exit(1)
-            }
+        if (answer.language === "other") {
+            console.log(chalk.red('Analysis in other languages is a work in progress'))
+            process.exit(1)
+        } else {
+            let language = Object.values(languages).find(lang => lang.choice_name = answer.language)
+
+            answer = await Prompt([{
+                type: 'multiselect',
+                name: 'rules',
+                message: 'Which rules would you like to apply?',
+                choices: language.rule_names
+            }])
+            
+            await language.operations({ files, enabled_rules: answer.rules })
         }
     } catch (e) {
         // Ctrl-c while waiting for a prompt throws an uninformative empty string
         // if that's the case, just quit normally
-        process.exit( e === "" ? 0 : 1)
+        if (e !== "") console.log(e)
+        process.exit(e === "" ? 0 : 1)
     }
 }
 
